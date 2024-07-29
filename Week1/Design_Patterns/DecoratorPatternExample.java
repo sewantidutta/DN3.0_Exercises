a0e1d7c3c639f21a20b7767d8cd7bb1e6da86b5e
sewantidutta <134003042+sewantidutta@users.noreply.github.com>
2024-07-30 03:40:58 +0530

    Add files via upload

---
+++ Week1/Design_Patterns/DecoratorPatternExample.java
@@ -0,0 +1,60 @@
+// Notifier.java
+interface Notifier {
+    void send(String message);
+}
+
+// EmailNotifier.java
+class EmailNotifier implements Notifier {
+    @Override
+    public void send(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+// NotifierDecorator.java
+ abstract class NotifierDecorator implements Notifier {
+    private Notifier notifier;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    @Override
+    public void send(String message) {
+        notifier.send(message);
+    }
+}
+
+// SMSNotifierDecorator.java
+class SMSNotifierDecorator extends NotifierDecorator {
+    public SMSNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+// SlackNotifierDecorator.java
+class SlackNotifierDecorator extends NotifierDecorator {
+    public SlackNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        System.out.println("Sending Slack notification: " + message);
+    }
+}
+public class DecoratorPatternExample {
+    public static void main(String[] args) {
+        Notifier emailNotifier = new EmailNotifier();
+        Notifier smsNotifier = new SMSNotifierDecorator(emailNotifier);
+        Notifier slackNotifier = new SlackNotifierDecorator(smsNotifier);
+        slackNotifier.send("Hello, world!");
+    }
+}
