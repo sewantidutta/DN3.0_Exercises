9bdf94ed93941d4ea6ff08b8aee64637d17d7026
sewantidutta <134003042+sewantidutta@users.noreply.github.com>
2024-08-17 01:49:50 +0530

    Delete Week_3_Exercises directory

--- Week_3_Exercises/02. Employee_Management_System-Creating Entities/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeeManagementSystem.repository;
-
-import com.example.EmployeeManagementSystem.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- Week_3_Exercises/02. Employee_Management_System-Creating Entities/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeeManagementSystem.repository;
-
-import com.example.EmployeeManagementSystem.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
